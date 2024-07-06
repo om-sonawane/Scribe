be9c2e89510c19934726a501ef3b5f3b719c7a40
OM SONAWANE <omsonawane03@gmail.com>
2024-07-06 16:41:11 +0530

    "add"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuComponent]
 })
 export class AppComponent {
   title = 'Scribe';
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+ 
+logo: string =
+"";
+disabledMyButton:boolean=false;
+
+  constructor() { }
+  sayHello(){
+    alert("Hello World");
+  }
+  ngOnInit() { }
 
 }
