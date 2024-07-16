85a17e21249a2f0b5be7518cf42d36e9b4316950
OM SONAWANE <omsonawane03@gmail.com>
2024-07-16 11:56:13 +0530

    "more changes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from "./menu/menu.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuComponent]
+    
 })
 export class AppComponent {
   title = 'Scribe';
